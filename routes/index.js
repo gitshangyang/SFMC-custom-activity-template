e75dcae04555896c8317ddc4d094042087b7237e
gitshangyang <64315449+gitshangyang@users.noreply.github.com>
2020-06-04 11:55:34 +0200

    d

--- routes/index.js
+++ routes/index.js
@@ -23,7 +23,6 @@ exports.index = function(req, res) {
 
 exports.login = function(req, res) {
 	console.log("req.body: ", req.body);
-	console.log("req.session.token: ", req.session.token);
 	res.redirect("/");
 };
 
